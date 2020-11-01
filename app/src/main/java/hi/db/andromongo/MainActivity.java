1b656a2d83f74ff2f17fe9ef1c1d6b7963bc131b
Hiura Kiyosi <hiura@nn.iij4u.or.jp>
2020-11-01 15:38:08 +0900

    hiMongo ver0.11

--- app/src/main/java/hi/db/andromongo/MainActivity.java
+++ app/src/main/java/hi/db/andromongo/MainActivity.java
@@ -1,46 +1,29 @@
 package hi.db.andromongo;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
 import hi.db.hiMonWorkerSample;
 import hi.db.hiMongo;
-import otsu.hiNote.*;
-
-import static hi.db.hiMonWorkerSample.*;
 class MongoCaller extends AsyncTask<String, Void, String>{
    private MainActivity mActivity;
    public MongoCaller(Activity activity) {mActivity =(MainActivity) activity; }
    @Override
-   protected String doInBackground(String... params_) {
-      StringBuilder         _sb =new StringBuilder();
-      hiMonWorkerSample.COM _com=new hiMonWorkerSample.COM(params_[0],8010,15);
-      hiMongo.DB db = hiMongo.use("db01",_com);
-      if( params_[1].equals("drop") ){
-         db.in("coll_01").drop();
+   protected String doInBackground(String... prm_) {
+      hiMonWorkerSample.COM _com=new hiMonWorkerSample.COM(prm_[0],8010,15);
+      hiMongo.Collection coll = hiMongo.use("db01",_com).in("coll_01");
+      if( prm_[1].equals("last_records") ){
+         coll.find().sort("{_id:-1}").forOne(Fi->{
+            Fi.deleteMany("{date:{$lt:{$calc:'#CUR.date-5000'}}}");
+            });
          }
-      else if( params_[1].equals("insert") ){
-         db.in("coll_01")
-           .insertOne(params_[2])
-           .find("{}","{_id:0}")
-           .sort("{_id:-1}")
-           .limit(4)
-           .getJsonList(hiU.REVERSE)
-           .forEach(Rj->_sb.append(Rj+"\n"))
-           ;
-         }
-      hiU.close(_com);
+      else if( prm_[1].equals("drop") )  coll.drop();
+      else if( prm_[1].equals("insert") )coll.insertOne(prm_[2]);
+      StringBuilder _sb =new StringBuilder();
+      coll.find("{}","{_id:0}").forEachJson(Rj->_sb.append(Rj+"\n"));
+      _com.close();
       return _sb.toString();
       }
    @Override
@@ -54,16 +37,21 @@ public class MainActivity extends AppCompatActivity{
    protected void onCreate(Bundle savedInstanceState){
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      findViewById(R.id.drop).setOnClickListener((_View_) ->{
-         MongoCaller _mongoCaller = new MongoCaller(this);
-         String _host=((EditText)findViewById(R.id.host)).getText().toString();
-         _mongoCaller.execute(_host,"drop");
-         });
       findViewById(R.id.insert).setOnClickListener((_View_) ->{
          MongoCaller _mongoCaller = new MongoCaller(this);
          String _host=((EditText)findViewById(R.id.host)).getText().toString();
          _mongoCaller.execute(_host,"insert"
                              ,"{type:'X',value:"+(++count)+",date:"+hiMongo.date()+"}");
+         });
+      findViewById(R.id.last_records).setOnClickListener((_View_) ->{
+         MongoCaller _mongoCaller = new MongoCaller(this);
+         String _host=((EditText)findViewById(R.id.host)).getText().toString();
+         _mongoCaller.execute(_host,"last_records");
+         });
+      findViewById(R.id.drop).setOnClickListener((_View_) ->{
+         MongoCaller _mongoCaller = new MongoCaller(this);
+         String _host=((EditText)findViewById(R.id.host)).getText().toString();
+         _mongoCaller.execute(_host,"drop");
          });
       }
    void dispResult(String result_){
